b85a70458a391d246867ef7ebac09361337c4d39
Fennel <fennel@cristata.local>
2024-07-14 03:25:01 -0400

    2015/1/2 done

--- 2015/1/main.c
+++ 2015/1/main.c
@@ -1,14 +1,18 @@
 #import <stdio.h>
 
 int main() {
-  int ch;
-  int val;
+  int ch = 0, val = 0, i = 0;
   while ((ch = getchar()) != EOF) {
+    if (val < 0) {
+      printf("%i\n", i);
+      return 0;
+    }
     if (ch == '(') {
       val++;
     } else if (ch == ')') {
       val--;
     }
+    i++;
   }
-  printf("%d", val);
+  return 1;
 }
