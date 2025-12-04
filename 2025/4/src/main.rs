210e7f5b71a5fb2cdeb661c27f4a5611bb1669d5
Fennel <fennel@everwild.dev>
2025-12-04 11:15:20 -0500

    in progress

---
+++ 2025/4/src/main.rs
@@ -0,0 +1,66 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn check_char(c: Option<&char>) -> u8 {
+    match c {
+        Some(d) => match d {
+            '@' => 1,
+            _ => 0,
+        },
+        None => 0,
+    }
+}
+
+fn single_row(buffer: &Vec<Vec<char>>) -> u32 {
+    let mut sum = 0u32;
+    for i in 0..buffer[1].len() {
+        let mut adjs = 0u8;
+        if i > 0 {
+            adjs += check_char(buffer[1].get(i - 1));
+            adjs += check_char(buffer[0].get(i - 1));
+            adjs += check_char(buffer[2].get(i - 1));
+        }
+        adjs += check_char(buffer[1].get(i + 1));
+        adjs += check_char(buffer[0].get(i));
+        adjs += check_char(buffer[0].get(i + 1));
+        adjs += check_char(buffer[2].get(i));
+        adjs += check_char(buffer[2].get(i + 1));
+        if adjs < 4 {
+            sum += 1;
+        }
+    }
+    sum
+}
+
+fn main() {
+    let input = File::open("input.txt").unwrap();
+    let mut input_lines = BufReader::new(input).lines();
+    let mut buffer: Vec<Vec<char>> = Vec::new();
+
+    // Setup buffer
+    buffer.extend(
+        input_lines
+            .by_ref()
+            .take(2)
+            .map(|e| e.unwrap().chars().collect()),
+    );
+    let length = buffer[0].len();
+    let row_padding = vec!['.'; length];
+    buffer.insert(0, row_padding);
+    let mut answer = 0u32;
+    answer += single_row(&buffer);
+
+    for line in input_lines {
+        buffer.remove(0);
+        buffer.push(line.unwrap().chars().collect());
+        answer += single_row(&buffer);
+    }
+
+    buffer.remove(0);
+    buffer.push(vec!['.'; length]);
+    answer += single_row(&buffer);
+
+    println!("Result: {answer}");
+}
