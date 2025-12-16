51c3569ba1ca7bafb9c4a3b5eb1e1e4a5ceaf2e6
Ethan Meltzer <ethan-meltzer@proton.me>
2025-12-15 21:40:01 -0600

    2025/6/2 done

--- 2025/6/src/main.rs
+++ 2025/6/src/main.rs
@@ -7,47 +7,68 @@ fn main() {
     let input = File::open("input.txt").unwrap();
     let lines = BufReader::new(input).lines();
 
-    let mut lines: Vec<Vec<String>> = lines
-        .map(|f| {
-            f.unwrap()
-                .to_owned()
-                .split_whitespace()
-                .map(str::to_string)
-                .collect::<Vec<_>>()
-        })
-        .collect();
-    println!("{lines:?}");
-
-    let operations = lines.pop().unwrap();
-
-    let lines = lines
-        .iter()
-        .map(|e| {
-            e.iter()
-                .map(|f| f.parse::<u64>().unwrap())
-                .collect::<Vec<_>>()
-        })
+    let mut char_matrix = lines
+        .map(|e| e.unwrap().chars().rev().collect::<Vec<_>>())
         .collect::<Vec<_>>();
 
-    assert_eq!(operations.len(), lines[0].len());
+    let mut operations = char_matrix.pop().unwrap().into_iter().filter(|e| *e != ' ');
 
     let mut answer = 0u64;
-    // Group all by column
-    for i in 0..lines[0].len() {
-        let (is_mul, operation) = match operations[i].as_str() {
-            "+" => (false, u64::strict_add as fn(u64, u64) -> u64),
-            "*" => (true, u64::strict_mul as fn(u64, u64) -> u64),
-            _ => panic!(),
+    let mut transpose: Vec<Vec<char>> = vec![];
+    for i in 0..char_matrix[0].len() {
+        // println!("Starting transpose: {transpose:?}");
+        let mut all_spaces = true;
+        transpose.push(vec![]);
+        for (j, _) in char_matrix.iter().enumerate() {
+            match char_matrix[j][i] {
+                e if e.is_ascii_digit() => {
+                    let l = transpose.len();
+                    transpose[l - 1].push(e);
+                    all_spaces = false;
+                }
+                _ => (),
+            }
+        }
+
+        if !all_spaces {
+            continue;
+        }
+
+        transpose.pop();
+
+        let nums = transpose.iter().map(|e| {
+            e.iter()
+                .fold(0u64, |acc, e| acc * 10 + e.to_digit(10).unwrap() as u64)
+        });
+        let (acc, operation) = match operations.next().unwrap() {
+            '+' => (0u64, u64::strict_add as fn(u64, u64) -> u64),
+            '*' => (1u64, u64::strict_mul as fn(u64, u64) -> u64),
+            e => panic!("Found unexpected {e} in operations"),
         };
 
-        let partial = lines
-            .iter()
-            .clone()
-            .fold(is_mul as u64, |acc, e| operation(acc, e[i]));
+        println!("Nums: {nums:?}");
 
-        println!("Partial {i}: {partial}");
+        let partial = nums.fold(acc, operation);
+        println!("Partial: {partial}");
         answer += partial;
+        transpose = vec![];
     }
+
+    let nums = transpose.iter().map(|e| {
+        e.iter()
+            .fold(0u64, |acc, e| acc * 10 + e.to_digit(10).unwrap() as u64)
+    });
+    let (acc, operation) = match operations.next().unwrap() {
+        '+' => (0u64, u64::strict_add as fn(u64, u64) -> u64),
+        '*' => (1u64, u64::strict_mul as fn(u64, u64) -> u64),
+        e => panic!("Found unexpected {e} in operations"),
+    };
+
+    println!("Nums: {nums:?}");
+
+    let partial = nums.fold(acc, operation);
+    println!("Partial: {partial}");
+    answer += partial;
 
     println!("Answer: {answer}");
 }
